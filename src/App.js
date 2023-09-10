b5f4fb1874c36305e80785e2b56a3b75e5daa71e
Bakhytzhan Abeldinov <bakhytzhan79@mail.ru>
2023-09-10 17:18:40 +0600

    Add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { padsBankRight, padsBankLeft } from "./data";
+import "./App.scss";
 
 function App() {
+  const [volume, setVolume] = useState(50);
+  const [power, setPower] = useState(true);
+  const [bank, setBank] = useState(false);
+  const [display, setDisplay] = useState();
+
+  const playSound = useCallback(
+    (selector) => {
+      const audio = document.getElementById(selector);
+      if (audio) {
+        const parent = audio.parentElement;
+        // audio.currentTime = 0;
+        if (power) {
+          setDisplay(parent.id);
+          audio.volume = volume / 100;
+          audio.play();
+
+          parent.setAttribute(
+            "style",
+            "background-color: orange; margin-top: 13px; box-shadow: 0px 3px orange; height: 77px"
+          );
+        } else {
+          parent.setAttribute(
+            "style",
+            "margin-top: 13px; box-shadow: 0px 3px gray; height: 77px"
+          );
+        }
+        setTimeout(() => {
+          parent.style.backgroundColor = null;
+          parent.style.marginTop = null;
+          parent.style.boxShadow = null;
+          parent.style.height = null;
+        }, 200);
+      }
+    },
+    [volume, power]
+  );
+
+  const keydownHandler = useCallback(
+    (event) => {
+      switch (event.key) {
+        case "Q":
+        case "W":
+        case "E":
+        case "A":
+        case "S":
+        case "D":
+        case "Z":
+        case "X":
+        case "C":
+          playSound(event.key);
+          break;
+        default:
+          return;
+      }
+    },
+    [playSound]
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", keydownHandler);
+    return () => window.removeEventListener("keydown", keydownHandler);
+  }, [keydownHandler]);
+
+  const pads = useMemo(() => {
+    return bank ? padsBankRight : padsBankLeft;
+  }, [bank]);
+
+  const powerHandler = () => {
+    setPower((prev) => !prev);
+  };
+
+  const volumeHandler = (event) => {
+    const volumeLevel = event.target.value;
+    setVolume(volumeLevel);
+    setDisplay(`Volume: ${volumeLevel}`);
+    setTimeout(() => setDisplay(""), 1000);
+  };
+
+  const bankSelectHandler = () => {
+    if (power) {
+      setBank((prev) => !prev);
+      setDisplay(bank ? "Heater Kit" : "Smooth Piano Kit");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inner-container" id="drum-machine">
+        <div className="pad-bank">
+          {pads.map((elem) => {
+            return (
+              <div
+                key={elem.id}
+                className="drum-pad"
+                id={elem.id}
+                onClick={() => playSound(elem.text)}
+              >
+                <audio src={elem.src} className="clip" id={elem.text}></audio>
+                {elem.text}
+              </div>
+            );
+          })}
+        </div>
+        <div className="logo">
+          <div className="inner-logo">FCC&nbsp;</div>
+          <i className="inner-logo fa fa-free-code-camp"></i>
+        </div>
+        <div className="controls-container">
+          <div className="control">
+            <p>Power</p>
+            <div className="select" onClick={powerHandler}>
+              <div
+                className="inner"
+                style={power ? { float: "right" } : { float: "left" }}
+              ></div>
+            </div>
+          </div>
+          <p id="display">{display}&nbsp;</p>
+          <div className="volume-slider">
+            <input
+              max="100"
+              min="0"
+              step="1"
+              type="range"
+              value={volume}
+              onChange={(event) => volumeHandler(event)}
+              disabled={!power}
+            />
+          </div>
+          <div className="control">
+            <p>Bank</p>
+            <div className="select" onClick={bankSelectHandler}>
+              <div
+                className="inner"
+                style={bank ? { float: "right" } : { float: "left" }}
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,95 @@
+export const padsBankLeft = [
+  {
+    text: "Q",
+    id: "Heater-1",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    text: "W",
+    id: "Heater-2",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    text: "E",
+    id: "Heater-3",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    text: "A",
+    id: "Heater-4",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    text: "S",
+    id: "Clap",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    text: "D",
+    id: "Open-HH",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+  {
+    text: "Z",
+    id: "Kick-n'-Hat",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    text: "X",
+    id: "Kick",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    text: "C",
+    id: "Closed-HH",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
+];
+
+export const padsBankRight = [
+  {
+    text: "Q",
+    id: "Chord-1",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+  },
+  {
+    text: "W",
+    id: "Chord-2",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+  },
+  {
+    text: "E",
+    id: "Chord-3",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+  },
+  {
+    text: "A",
+    id: "Shaker",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+  },
+  {
+    text: "S",
+    id: "Open-HH",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+  },
+  {
+    text: "D",
+    id: "Closed-HH",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+  },
+  {
+    text: "Z",
+    id: "Punchy-Kick",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+  },
+  {
+    text: "X",
+    id: "Side-Stick",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+  },
+  {
+    text: "C",
+    id: "Snare",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+  },
+];
